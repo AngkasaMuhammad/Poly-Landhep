37626a62ccb712a5ec7492427eb18aeaf76ed351
AngkasaMuhammad <80026325+AngkasaMuhammad@users.noreply.github.com>
2025-08-28 09:43:35 +0700

    Add files via upload

---
+++ Poly Landhep/v4.2/js/main.js
@@ -0,0 +1,700 @@
+'use strict'
+
+/*
+import {
+	vec3 as v3,
+	mat4 as m4,
+} from 'https://wgpu-matrix.org/dist/3.x/wgpu-matrix.module.js'
+*/
+let v3 = wgpuMatrix.vec3
+let m4 = wgpuMatrix.mat4
+
+
+
+
+import {
+	lihat as lih,
+	sleep,
+	tyarr_b64,
+	b64_tyarr,
+	pisahstr,
+	//getWordByIndex,
+} from '../../v4/js/utilku.js'
+
+
+
+import {
+	canv3d,
+	canv2d,
+	cx3d,
+	cx2d,
+	tambahinfo,
+	
+	uiloop,
+	
+	set_getsuara,
+	set_freecam,
+	set_setspf,
+} from './ui.js'
+
+
+
+import {
+	loadcam,
+	fview,
+} from './camera.js'
+
+
+let valtype = val=>{
+    if (val === null) {
+        return 'null'; // Special case: typeof null is "object", so handle it explicitly.
+    }
+    if (Array.isArray(val)) {
+        return 'array'; // Arrays are technically objects, but we want to distinguish them.
+    }
+    const type = typeof val;
+    if (type === 'object') {
+        return 'object'; // Handle plain objects.
+    }
+    return type; // Return the type for primitives like 'number', 'string', 'boolean', etc.
+}
+
+let context = cx3d
+let presentationFormat = navigator.gpu.getPreferredCanvasFormat()
+
+let aucx = new AudioContext()
+let suara = null
+export let getsuara = ()=>suara
+set_getsuara(getsuara)
+
+
+
+let urlparam = new URLSearchParams(location.search)
+
+let resosrclink = new URL(urlparam.get('resource') ?? 'resources.json',location.href,) //
+let resosrc = fetch(resosrclink)
+.then(r=>r.json())
+
+let encosrclink = new URL(urlparam.get('encoder') ?? 'encoder.json',location.href,) //
+let encosrc = fetch(encosrclink)
+.then(r=>r.json())
+
+let contsrclink = new URL(urlparam.get('controller') ?? 'controller.json',location.href,) //
+let contsrc = fetch(contsrclink)
+.then(r=>r.json())
+
+let dv = navigator.gpu.requestAdapter()
+.then(adap=>adap.requestDevice({
+	requiredFeatures: ['indirect-first-instance'], // Enable the feature here
+}))
+
+let reso = new Map()
+let encoarr
+let cont
+
+
+
+let cvd = {label:'canvas color view '+Math.random()}//canvas view descriptor
+let _aucon = class {
+	constructor(audioData) {
+		let curtime = aucx.currentTime
+		this.audioContext = aucx;
+		this.audioData = audioData;
+		this.sources = [];
+		this.gainNodes = [];
+		this.isPlaying = false;
+		this.speed = 1;
+		this.startTime = curtime;// global
+		this.curtime = 0;// global, ga realtime
+		this.lastseek = 0;// local
+		this.stopTime = curtime;// global
+	}
+
+	createSource(audioBuffer, volume, loop,newspeed,) {
+		const source = this.audioContext.createBufferSource();
+		source.buffer = audioBuffer;
+		source.playbackRate.value = newspeed;
+		source.loop = loop;
+
+		const gainNode = this.audioContext.createGain();
+		gainNode.gain.value = volume;
+
+		source.connect(gainNode).connect(this.audioContext.destination);
+
+		return { source, gainNode };
+	}
+
+	start_at(seek,newspeed,) {
+		if (this.isPlaying) return;
+		this.isPlaying = true;
+		
+		let curtime = this.audioContext.currentTime
+		
+		let newstartTime = curtime
+		
+		this.audioData.forEach(({ src, when, offset, duration, volume, loop, myspeed = 1,}) => {
+			//const { source, gainNode } = this.createSource(src, volume, loop,newspeed,); ASLI
+			const { source, gainNode } = this.createSource(src, volume, loop,newspeed*myspeed,); //COBA
+			
+			
+			let whenglo	= newstartTime + (when - seek)/newspeed	;	;whenglo = Math.max(0,whenglo,)
+			//let offsetlok	= Math.max(offset,offset + seek - when,)	;	;offsetlok = Math.max(0,offsetlok,) ASLI
+			let offsetlok	= Math.max(offset,offset + (seek - when)*myspeed,)	;	;offsetlok = Math.max(0,offsetlok,) //COBA
+			
+			if(duration === null){
+//cobaan, 
+if(loop){
+	offsetlok %= src.duration
+}
+//
+				source.start(whenglo,offsetlok,)
+			}else{
+				let durglo = Math.min(duration,duration + when - seek,) ;durglo = Math.max(0,durglo,)
+				source.start(whenglo,offsetlok,durglo,)
+			}
+			this.sources.push(source);
+			this.gainNodes.push(gainNode);
+			
+		});
+		//akhir
+		this.speed = newspeed
+		this.startTime = curtime
+		this.lastseek = seek
+
+	}
+	
+	getCurTime() {//realtime
+		let curtime = this.audioContext.currentTime
+		return this.isPlaying
+		? (
+			(this.isPlaying ? curtime : this.stopTime)
+			- this.startTime
+		)*this.speed + this.lastseek
+		: this.curtime
+	}
+
+	destroy() {
+		if (!this.isPlaying) return
+		this.curtime = this.getCurTime()
+		this.isPlaying = false;
+		this.stopTime = this.audioContext.currentTime
+		this.sources.forEach((source) => {
+			source.stop();
+			source.disconnect();
+		});
+		this.gainNodes.forEach((gainNode) => gainNode.disconnect());
+		this.sources = [];
+		this.gainNodes = [];
+	}
+	
+	//lainlain
+	play(){
+		//"this.curtime" beda dengan "curtime"
+		this.start_at(this.curtime,this.speed,)
+	}
+	pause(){
+		this.destroy()
+	}
+	setCurTime(time){
+		if(this.isPlaying){
+			this.destroy()
+			this.start_at(time,this.speed,)
+		}else{
+			this.curtime = time
+		}
+	}
+	setspeed(speed){
+		if(this.isPlaying){
+			this.destroy()
+			this.start_at(this.curtime,speed,)
+		}else{
+			this.speed = speed
+		}
+	}
+};
+
+addEventListener('mousedown',e=>aucx.resume(),)
+
+let misc = new ArrayBuffer((
+	+16 //perspective
+	+16 //camera pivot
+	+16 //camera
+	+16 //camera inverse
+	+16 //camera view
+	+1 //time now
+	+1 //seek
+	+1 //free cam
+	+1 //previous seek
+	+1 //random float
+	+3 // (pad)
+)*4)
+let miof = 0 //misc offset
+let fmiof = off=>{ //offset
+	let cur = miof
+	miof += off
+	return cur
+}
+
+let persp = new Float32Array(misc,( fmiof(16) )*4,16,)
+let pivot = new Float32Array(misc,( fmiof(16) )*4,16,)
+let cam = new Float32Array(misc,( fmiof(16) )*4,16,)
+let invcam = new Float32Array(misc,( fmiof(16) )*4,16,)
+let view = new Float32Array(misc,( fmiof(16) )*4,16,)
+let now = new Uint32Array(misc,( fmiof(1) )*4,1,)
+let seek = new Float32Array(misc,( fmiof(1) )*4,1)
+export let freecam = new Uint32Array(misc,( fmiof(1) )*4,1)
+let prevseek = new Float32Array(misc,( fmiof(1) )*4,1)
+let ranfl = new Float32Array(misc,( fmiof(1) )*4,1)
+set_freecam(freecam)
+fview((
+	_persp,
+	_pivot,
+	_cam,
+	_invcam,
+	_view,
+)=>{
+	m4.copy(_persp,persp,)
+	m4.copy(_pivot,pivot,)
+	m4.copy(_cam,cam,)
+	m4.copy(_invcam,invcam,)
+	m4.copy(_view,view,)
+})
+let miscbuf
+
+let create_gpu_object = new Map()
+
+create_gpu_object.set(
+'gpu_buffer',async ({
+	type,
+	descriptor:descr, //d_
+	data, //da_
+})=>{
+	await 0 //lih(type)
+	await 0 //lih(type)
+	
+	//let d_obj
+	let d_key = (valtype(descr) === 'string') && (reso.get(descr) !== undefined)
+	let d_link = (valtype(descr) === 'string') && (reso.get(descr) === undefined)
+	//let da_null
+	let da_link = data !== null
+	
+	d_key && (descr = await reso.get(descr))
+	d_link && (descr = fetch(new URL(descr,resosrclink,)))
+	d_link && (descr = (await descr).json())
+	da_link && (data = fetch(new URL(data,resosrclink,)))
+	da_link && (data = (await data).arrayBuffer())
+	
+/*========
+	descr = fetch(new URL(descr,resosrclink,))
+	data = (data === null)?null:fetch(new URL(data,resosrclink,))
+	
+	descr = (await descr).json()
+	data = (await data)?.arrayBuffer?.()??null
+---------*/
+	
+	let buf = dv.createBuffer(await descr)
+	da_link && dv.queue.writeBuffer(buf,0,await data,)
+	return buf
+},)
+
+create_gpu_object.set(
+'gpu_texture',async ({
+	type,
+	descriptor:descr,
+	data,
+})=>{
+	await 0 //lih(type)
+	await 0 //lih(type)
+	
+	let ibm = null //image bitmap
+	if(data !== null){
+		let img = document.createElement("img")
+		img.crossOrigin = "anonymous" // Enable CORS
+		img.src = new URL(data,resosrclink,).toString()
+		await img.decode()
+		ibm = await createImageBitmap(img)
+		
+	}
+	
+	if(texsize.has(descr.size)){
+		descr.size = texsize.get(descr.size)(ibm)
+	}
+	let tex = dv.createTexture(descr)
+	
+	if(data !== null){	
+		dv.queue.copyExternalImageToTexture(
+			{ source: ibm},
+			{ texture: tex},
+			[ibm.width, ibm.height],
+		)
+	}
+	
+	return tex
+},)
+
+
+
+let texsize = new Map()//texture size
+
+texsize.set(
+	'(canvas)',
+	()=>canv3d, //ambil width & height doang
+)
+texsize.set(
+	'(image_data)',
+	ibm=>[ibm.width,ibm.height,], //ambil width & height doang
+)
+
+create_gpu_object.set(
+'gpu_texture_view',async ({
+	type,
+	descriptor:descr,
+	data,
+})=>{
+	await 0 //lih(type)
+	await 0 //lih(type)
+	
+	let view = (await reso.get(data)).createView(descr)
+	return view
+},)
+
+
+
+let texview = new Map()//texture view
+texview.set(
+	'(context)',
+	()=>context.getCurrentTexture().createView(cvd),
+)
+
+create_gpu_object.set(
+'audio_buffer',async ({
+	type,
+	descriptor:descr,
+	data,
+})=>{
+	await 0 //lih(type)
+	await 0 //lih(type)
+	
+	//sampe sini, audio_buffer
+	const response = await fetch(new URL(data,resosrclink,));
+	const arrayBuffer = await response.arrayBuffer();
+	let aubuf = await aucx.decodeAudioData(arrayBuffer);
+	
+	return aubuf
+},)
+
+create_gpu_object.set(
+'gpu_render_pipe',async ({
+	type,
+	descriptor:descr,
+	data,
+})=>{
+	await 0 //lih(type)
+	await 0 //lih(type)
+	
+//fragment module
+	descr.fragment.module =
+	await reso
+	.get(descr.fragment.module)
+	
+//vertex module
+	descr.vertex.module =
+	await reso
+	.get(descr.vertex.module)
+	
+//format
+	for(let target of descr.fragment.targets){
+		if(pf.has(target.format)){
+			target.format = pf.get(target.format)
+		}
+	}
+	
+//buffer
+	descr.vertex.buffers = await Promise.all(
+		descr.vertex.buffers
+		.map(str=>
+			(valtype(str) === 'string')
+			?
+			fetch(new URL(str,resosrclink,))
+			.then(res=>res.json())
+			:
+			str
+		)
+	)
+	
+	
+	return await dv.createRenderPipelineAsync(descr)
+},)
+
+
+
+let pf = new Map() //pipe format
+pf.set(
+	'(preferred_canvas_format)',
+	navigator.gpu.getPreferredCanvasFormat(),
+)
+
+create_gpu_object.set(
+'gpu_shader_module',async ({
+	type,
+	descriptor:descr,
+	data,
+})=>{
+	await 0 //lih(type)
+	await 0 //lih(type)
+	
+	descr.code = fetch(new URL(descr.code,resosrclink,))
+	descr.code = (await descr.code).text()
+	descr.code = await descr.code
+	return dv.createShaderModule(descr)
+},)
+
+create_gpu_object.set(
+'gpu_buffer_binding',async ({
+	type,
+	descriptor:descr,
+	data,
+})=>{
+	await 0 //lih(type)
+	await 0 //lih(type)
+	
+	
+	descr.buffer = 
+	bbf.has(descr.buffer)
+	?bbf.get(descr.buffer)
+	:await reso.get(descr.buffer)
+	
+	return descr
+},)
+
+
+
+let bbf //buffer binding format
+
+create_gpu_object.set(
+'gpu_bind_group',async ({
+	type,
+	descriptor:descr,
+	data,
+})=>{
+	await 0 //lih(type)
+	await 0 //lih(type)
+	
+	for(let entry of descr.entries){
+		entry.resource = await reso.get(entry.resource)
+	}
+	descr.layout = (await reso.get(descr.layout.pipe))
+	.getBindGroupLayout(descr.layout.layout)
+	
+	return dv.createBindGroup(descr)
+},)
+
+let mepa = new Map()//method param
+
+mepa.set(
+'set_pipeline',
+async met=>{
+	met[0] = await reso.get(met[0])
+	return {
+		'method':'setPipeline',
+		'param':met,
+	}
+},)
+
+mepa.set(
+'set_vertex_buffer',
+async met=>{
+	met[1] = await reso.get(met[1])
+	return {
+		'method':'setVertexBuffer',
+		'param':met,
+	}
+},)
+
+mepa.set(
+'set_index_buffer',
+async met=>{
+	met[0] = await reso.get(met[0])
+	return {
+		'method':'setIndexBuffer',
+		'param':met,
+	}
+},)
+
+mepa.set(
+'set_bind_group',
+async met=>{
+	met[1] = await reso.get(met[1])
+	return {
+		'method':'setBindGroup',
+		'param':met,
+	}
+},)
+
+
+mepa.set(
+'draw_indexed_indirect',
+async met=>{
+	met[0] = await reso.get(met[0])
+	return {
+		'method':'drawIndexedIndirect',
+		'param':met,
+	}
+},)
+
+
+mepa.set(
+'draw',
+async met=>{
+	return {
+		'method':'draw',
+		'param':met,
+	}
+},)
+
+
+mepa.set(
+'draw_indirect',
+async met=>{
+	met[0] = await reso.get(met[0])
+	return {
+		'method':'drawIndirect',
+		'param':met,
+	}
+},)
+
+
+export let main = async ()=>{
+
+resosrc = await resosrc
+dv = await dv
+
+context.configure({
+	device:dv,
+	format: presentationFormat,
+});
+
+loadcam()
+
+miscbuf = dv.createBuffer({
+	label: 'innii Uniform buffer '+Date()+'_'+Math.random().toFixed(13),
+	size: misc.byteLength,
+	usage:
+		GPUBufferUsage.UNIFORM
+		| GPUBufferUsage.COPY_SRC
+		| GPUBufferUsage.COPY_DST,
+})
+bbf = new Map()//buffer binding format
+bbf.set(
+	'(misc)',
+	miscbuf,
+)
+
+
+lih(resosrc)
+for(let key in resosrc.create){
+	let info = resosrc.create[key]
+	reso.set(key,
+		create_gpu_object
+		.get(info.type)
+		?.(info),
+	)
+}
+lih(reso)
+
+encoarr = await encosrc
+for(let enco of encoarr){
+	for(let rp of enco.renderpasses){
+		for(let ca of rp.descriptor.colorAttachments){
+			ca.view = texview.has(ca.view)
+			?texview.get(ca.view)()
+			:await reso.get(ca.view)
+		}
+		let dsa = rp.descriptor.depthStencilAttachment
+		dsa.view = await reso.get(dsa.view)
+		
+		for(let i = 0;i < rp.methods.length;i++){
+			let met = rp.methods[i]
+			rp.methods[i] = await mepa.get(met.shift())(met)
+		}
+	}
+}
+lih(encoarr)
+
+
+cont = await contsrc
+lih(cont)
+for(let sound of cont){
+	sound.src = await reso.get(sound.src)
+}
+suara = new _aucon(cont)
+lih(suara)
+suara.play()
+
+let waitgpu = null
+let draw = async t=>{
+	let submit = []
+	for(let {
+		descriptor:descr,
+		renderpasses:rp,
+	} of encoarr){
+		let enco = dv.createCommandEncoder(descr)
+		for(let {
+			descriptor:descr,
+			methods,
+		} of rp){
+			for(let ca of descr.colorAttachments){
+				if(ca.view.label === cvd.label){
+					ca.view = context.getCurrentTexture().createView(cvd)
+				}
+			}
+			
+			let pass = enco.beginRenderPass(descr)
+			for(let met of methods){
+				pass[met.method](...met.param)
+			}
+			pass.end()
+		}
+		submit.push(enco.finish())
+	}
+	dv.queue.submit(submit)
+	waitgpu = dv.queue.onSubmittedWorkDone()
+}
+
+let step = 0
+let spf = 1 //steps per frame
+set_setspf(val=>spf = val)
+
+let loop = async t=>{
+	now[0] = Math.round(performance.now())
+	if(step-- <= 1){
+		prevseek[0] = seek[0]
+		seek[0] = suara.getCurTime()
+		ranfl[0] = Math.random()
+		
+		step = spf
+		await waitgpu //taruh di sebelum createView(); destroyed karena nunggu
+		dv.queue.writeBuffer(miscbuf,0,misc,)
+		draw(t)
+	}
+	
+	requestAnimationFrame(loop)
+	uiloop(t)
+}
+requestAnimationFrame(loop)
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+}
---
+++ Poly Landhep/v4.2/js/ui.js
@@ -0,0 +1,255 @@
+"use strict"
+/*
+RENCANA:
+=>	updbuf
+--	reload row manual
+	
+*/
+
+import {
+	lihat as lih,
+	que,
+	attr,
+} from '../../v4/js/utilku.js'
+/*
+import {
+	getsuara,
+	freecam, //dari misc
+} from './main.js'
+*/
+let getsuara = null	;export let set_getsuara = a=>getsuara = a
+let freecam = null	;export let set_freecam = a=>freecam = a
+let setspf = null	;export let set_setspf = a=>setspf = a
+
+
+
+
+let fSH = e=>{
+	let cur = e.currentTarget
+	let n = cur.classList.toggle('nyala')
+	let c = que(attr(cur,'sasaran',))[0].classList
+	let s = 'hidechild'
+	c.contains(s)?c.remove(s):c.add(s)
+	return c.contains(s)
+}
+let fSHHelp = e=>{
+	camlocked = fSH(e)
+	freecam[0] = +!camlocked
+}
+
+que('#SHhelp')[0].addEventListener('click',fSHHelp,)
+que('#SHcontroller')[0].addEventListener('click',fSH,)
+que('#SHinfo')[0].addEventListener('click',fSH,)
+
+/*=-=-=-=-=-=-=-=-
+
+
+
+
+
+
+
+
+
+
+=-=-=-=-=-=-=-=-*/
+let play = e=>getsuara().play()
+let pause = e=>getsuara().pause()
+let seekdown = e=>e.currentTarget.requestPointerLock({unadjustedMovement: true,})
+let seekmove = e=>{
+	if(document.pointerLockElement !== e.currentTarget){ return }
+	getsuara().setCurTime(Math.max(0,getsuara().getCurTime()+ +e.movementX/44*getsuara().speed),)
+}
+let seekup = e=>document.exitPointerLock()
+//let speed = e=>getsuara().setspeed(+e.currentTarget.value)
+let speeddown = e=>e.currentTarget.requestPointerLock({unadjustedMovement: true,})
+let speedmove = e=>{
+	if(document.pointerLockElement !== e.currentTarget){ return }
+	let v = getsuara().speed
+	let fac = 1.08
+	v = Math.log(v)/Math.log(fac)
+		v += +e.movementX/44
+	v = fac**v
+	getsuara().setspeed(v)
+}
+let speedup = e=>document.exitPointerLock()
+let fps = e=>{
+	let ele = que('#fps'	)[0]
+	let stepcount = +attr(ele,'stepcount',)
+	stepcount = (stepcount < 6)?(stepcount+1):1
+	attr(ele,'stepcount',stepcount,)
+	setspf(stepcount)
+	ele.firstElementChild.textContent = 60/stepcount
+}
+let seekreset = e=>getsuara().setCurTime(0)
+let speedreset = e=>getsuara().setspeed(1)
+
+export let uiloop = async t=>{
+	//lih(333)
+	que('#showseek')[0].textContent = getsuara().getCurTime().toFixed(3)
+	que('#showspeed')[0].textContent = getsuara().speed.toFixed(3)
+	
+	que('#pesanselesai')[0].classList[
+		(getsuara().getCurTime() < 43)?'add':'remove'
+	]('hilang')
+}
+/*=-=-=-=-=-=-=-=-
+
+
+
+
+
+
+
+
+
+
+=-=-=-=-=-=-=-=-*/
+//lain - lain
+que('#play'	)[0].addEventListener('click'	,play	,)
+que('#pause'	)[0].addEventListener('click'	,pause	,)
+que('#seek'	)[0].addEventListener('mousedown'	,seekdown	,)
+que('#seek'	)[0].addEventListener('mousemove'	,seekmove	,)
+que('#seek'	)[0].addEventListener('mouseup'	,seekup	,)
+que('#speed'	)[0].addEventListener('mousedown'	,speeddown	,)
+que('#speed'	)[0].addEventListener('mousemove'	,speedmove	,)
+que('#speed'	)[0].addEventListener('mouseup'	,speedup	,)
+que('#fps'	)[0].addEventListener('click'	,fps	,)
+que('#showseek'	)[0].addEventListener('dblclick'	,seekreset	,);		que('#showseek')[0].textContent = '----'
+que('#showspeed'	)[0].addEventListener('dblclick'	,speedreset	,);		que('#showspeed')[0].textContent = '----'
+
+let svg = (queini,f,)=>{//copy gambar tongsampah
+	let del = que(queini)[0].cloneNode(true)
+	del.classList.remove('hilang')
+	del.id = ''
+	;(f !== null) && del.addEventListener('click',f,)
+	return del
+}
+let infolist = que('#infolist')[0]
+let clearinfo = que('#clearinfo')[0]
+let infolistcla = infolist.classList
+export let tambahinfo = (i,warna,infokey,)=>{
+	i =
+	i?.stack ??
+	i
+	
+	let cek = Math.random()
+	let adaele
+	if((infokey ?? cek) !== cek){//apakah infoke berupa null atau undefined
+		adaele	= que(`#infolist [infokey="${infokey}"`)[0]
+	}
+	if(adaele){
+		//infolistcla.add('infoupdate')
+		adaele.style.background = `rgba(66,66,66,${Math.random()})`
+		let t = Date.now()
+		if(4 < t-attr(adaele,'timestamp',)){// biar ga spam info
+			adaele.textContent = ''
+		}
+		attr(adaele,'timestamp',t,)
+		adaele.textContent += i+'\n'
+		return 0
+	}
+	let div = document.createElement('div')
+		div.textContent = i
+		div.style.color = warna
+		div.classList.add('baru')
+		//div.classList.add('bukainfo')
+		div.addEventListener('click',f_info,)
+		attr(div,'infokey',infokey,)
+		attr(div,'timestamp',Date.now(),)
+	let inli = que('#infolist')[0]
+	inli.appendChild(div)
+	//;(11 <= inli.childElementCount) && inli.removeChild(inli.firstElementChild)
+}
+let f_info = e=>{
+	let div = e.currentTarget
+	div.classList.remove('baru')
+	if(!(e.detail%2)){
+		div.classList.toggle('bukainfo')
+	}
+}
+let camlocked = true
+export let fcamlocked = ()=>camlocked
+
+clearinfo.addEventListener('click',e=>{
+	infolist.textContent = ''
+},)
+
+//setInterval(()=>infolistcla.remove('infoupdate'),1111,)
+setInterval(()=>{
+	for(let ele of infolist.children){
+		ele.style.background = ''
+	}
+},1111,)
+addEventListener('load',()=>tambahinfo(
+`Selamat datang di Poly Landhep v4.2, silakan -->> DOUBLECLICK <<-- info ini (expand & collapse)
+
+--------------------------
+sementara tidak ada info
+
+`,'cyan',),)
+/*=-=-=-=-=-=-=-=-
+
+
+
+
+
+
+
+
+
+
+=-=-=-=-=-=-=-=-*/
+	//mousetext
+	
+	let datang = e=>{
+		let s = attr(e.target,'mousedescr',)
+		moutex.textContent = JSON.parse(`"${s}"`)
+	}
+	let pergi = e=>moutex.textContent = ''
+	
+	let moutex = que('#moutex')[0]
+	/*
+	for(let ele of que('[mousedescr]')){
+		ele.addEventListener('mouseenter',datang,)
+		ele.addEventListener('mouseleave',pergi,)
+	}
+	*/
+	let berimouseevent = e=>{
+		if(//kasih event listener
+			attr(e.target,'mousedescr',) &&
+			!e.target.onmouseenter
+		){
+			;(e.target.onmouseenter = datang)({target:e.target})
+			e.target.onmouseleave = pergi
+		}
+	}
+	addEventListener('mousedown',berimouseevent,)
+	addEventListener('mousemove',e=>{
+		berimouseevent(e)
+		let x = Math.min(e.clientX+11,innerWidth-moutex.clientWidth,)
+		let y = Math.min(e.clientY+11,innerHeight-moutex.clientHeight,)
+		moutex.style.left = x+'px'
+		moutex.style.top = y+'px'
+	})
+	
+	//canvas resize
+	export let canv3d = document.querySelector('#canv3d')
+	export let canv2d = document.querySelector('#canv2d')
+	export let cx3d = canv3d.getContext('webgpu')
+	export let cx2d = canv2d.getContext('2d')
+	let canvres = e=>{
+		let s3d = canv3d.style
+		let s2d = canv2d.style
+		if(innerWidth/innerHeight > canv3d.width/canv3d.height){
+			s3d.height = '100%'
+			s3d.width = ''
+		}else{
+			s3d.height = ''
+			s3d.width = '100%'
+		}
+		s2d.height = s3d.height
+		s2d.width = s3d.width
+	}
+	addEventListener('resize',canvres,)
+	canvres()
